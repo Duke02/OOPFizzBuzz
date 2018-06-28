cb4fc50fc0dcf066bdb778e02249c8bb7aa6833a
trystan.may <trystan.may@wright.edu>
2018-06-27 20:41:19 -0400

    Added original FizzBuzz game.
    Signed-off-by: trystan.may <trystan.may@wright.edu>

--- main.cpp
+++ main.cpp
@@ -1,6 +1,16 @@
 #include <iostream>
+#include "OOPFizzBuzz.h"
 
 int main ( ) {
-	std::cout << "Hello, World!" << std::endl;
+
+	OOPFizzBuzz fizzBuzz;
+
+	fizzBuzz.add( 3, "Fizz" );
+	fizzBuzz.add( 5, "Buzz" );
+
+	for ( int i = 1; i <= 30; i++ ) {
+		std::cout << fizzBuzz.print( i ) << std::endl;
+	}
+
 	return 0;
 }
