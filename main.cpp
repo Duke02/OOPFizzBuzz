5db58c8b17866f3aba033e084be4989cb78dfa4d
trystan.may <trystan.may@wright.edu>
2018-06-27 20:45:18 -0400

    Added some more fun!
    Signed-off-by: trystan.may <trystan.may@wright.edu>

--- main.cpp
+++ main.cpp
@@ -5,10 +5,22 @@ int main ( ) {
 
 	OOPFizzBuzz fizzBuzz;
 
+	// Original game of FizzBuzz.
 	fizzBuzz.add( 3, "Fizz" );
 	fizzBuzz.add( 5, "Buzz" );
 
 	for ( int i = 1; i <= 30; i++ ) {
+		std::cout << fizzBuzz.print( i ) << std::endl;
+	}
+
+	std::cout << "======================================" << std::endl;
+
+	// Now for some fun!
+	fizzBuzz.add( 10, "Oh" );
+	fizzBuzz.add( 15, "Boy" );
+
+	// Original game of FizzBuzz.
+	for ( int i = 1; i <= 60; i++ ) {
 		std::cout << fizzBuzz.print( i ) << std::endl;
 	}
 
